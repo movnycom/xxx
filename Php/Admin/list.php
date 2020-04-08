70b66fa1bcad5968e044a23b8a84a83e42b10b79
movnycom <63178430+movnycom@users.noreply.github.com>
2020-04-08 18:39:21 +0800

    Add files via upload

---
+++ Php/Admin/list.php
@@ -0,0 +1,116 @@
+      <div class="tpl-left-nav tpl-left-nav-hover">
+        <div class="tpl-left-nav-title">管理列表</div>
+        <div class="tpl-left-nav-list">
+          <ul class="tpl-left-nav-menu">
+            <li class="tpl-left-nav-item">
+              <a href="?Php=Home/index" class="nav-link active">
+                <i class="am-icon-home"></i>
+                <span>首页</span></a>
+            </li>
+			<li class="tpl-left-nav-item">
+				<a href="javascript:;" class="nav-link tpl-left-nav-link-list">
+					 <i class="am-icon-cogs"></i>
+					<span>基本设置</span>
+					<i class="am-icon-angle-right tpl-left-nav-more-ico am-fr am-margin-right tpl-left-nav-more-ico-rotate"></i>
+				</a>
+				 <ul class="tpl-left-nav-sub-menu">
+
+					<li>
+						<a href="?Php=Home/Basic/Basicsetup" >
+							<i class="am-icon-angle-right"></i>
+							<span>基本设置</span>
+							<i class="am-icon-star tpl-left-nav-content-ico am-fr am-margin-right"></i>
+						</a>
+					</li>
+					<li>
+						<a href="?Php=Home/Basic/Statistics" >
+							<i class="am-icon-angle-right"></i>
+							<span>流量统计设置</span>
+							<i class="am-icon-star tpl-left-nav-content-ico am-fr am-margin-right"></i>
+						</a>
+					</li>				
+				</ul>
+            </li>				
+			<li class="tpl-left-nav-item">
+				<a href="javascript:;" class="nav-link tpl-left-nav-link-list">
+					 <i class="am-icon-expeditedssl"></i>
+					<span>安全管理</span>
+					<i class="am-icon-angle-right tpl-left-nav-more-ico am-fr am-margin-right tpl-left-nav-more-ico-rotate"></i>
+				</a>
+				 <ul class="tpl-left-nav-sub-menu">
+
+
+					<li>
+						<a href="?Php=Home/Security/UserPass" >
+							<i class="am-icon-angle-right"></i>
+							<span>登录重置</span>
+							<i class="am-icon-star tpl-left-nav-content-ico am-fr am-margin-right"></i>
+						</a>
+					</li>
+					
+				</ul>
+            </li>			
+			
+			<li class="tpl-left-nav-item">
+				<a href="javascript:;" class="nav-link tpl-left-nav-link-list">
+					<i class="am-icon-sitemap"></i>
+					<span>广告设置</span>
+					<i class="am-icon-angle-right tpl-left-nav-more-ico am-fr am-margin-right tpl-left-nav-more-ico-rotate"></i>
+				</a>
+				 <ul class="tpl-left-nav-sub-menu">
+
+					<li>
+						<a href="?Php=Home/Ad/Top" >
+							<i class="am-icon-angle-right"></i>
+							<span>头部横幅广告</span>
+							<i class="am-icon-star tpl-left-nav-content-ico am-fr am-margin-right"></i>
+						</a>
+					</li>
+					<li>
+						<a href="?Php=Home/Ad/Video" >
+							<i class="am-icon-angle-right"></i>
+							<span>详情与内容横幅广告</span>
+							<i class="am-icon-star tpl-left-nav-content-ico am-fr am-margin-right"></i>
+						</a>
+					</li>
+					<li>
+						<a href="?Php=Home/Ad/Couplets" >
+							<i class="am-icon-angle-right"></i>
+							<span>对联展现广告</span>
+							<i class="am-icon-star tpl-left-nav-content-ico am-fr am-margin-right"></i>
+						</a>
+					</li>
+					<li>
+						<a href="?Php=Home/Ad/Float" >
+							<i class="am-icon-angle-right"></i>
+							<span>底部浮漂广告</span>
+							<i class="am-icon-star tpl-left-nav-content-ico am-fr am-margin-right"></i>
+						</a>
+					</li>
+					<li>
+						<a href="?Php=Home/Ad/AdJs" >
+							<i class="am-icon-angle-right"></i>
+							<span>广告联盟JS广告</span>
+							<i class="am-icon-star tpl-left-nav-content-ico am-fr am-margin-right"></i>
+						</a>
+					</li>				
+				</ul>
+            </li>			
+
+
+
+	
+			
+			
+
+
+			
+            <li class="tpl-left-nav-item">
+              <a href="?Php=Home/Ad/IeUrl" class="nav-link tpl-left-nav-link-list" >
+                <i class="am-icon-gg"></i>
+                <span>友链管理</span>
+                <i class="tpl-left-nav-content tpl-badge-danger">友链</i></a>
+            </li>		
+          </ul>
+        </div>
+      </div>
